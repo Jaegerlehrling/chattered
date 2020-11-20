95668d589f6a29a91b35d0f0fbec9a3e496ef7ed
Jaegerlehrling <dencelebi@gmail.com>
2020-11-20 16:19:26 +0100

    Hey, sweetie

--- src/ChatteredHttpHandler.java
+++ src/ChatteredHttpHandler.java
@@ -4,6 +4,8 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 
@@ -11,24 +13,34 @@ public class ChatteredHttpHandler implements HttpHandler {
     @Override
     public void handle ( HttpExchange exchange ) throws IOException
     {
+        String response = "Server is running.";
+        List<String> chat = new LinkedList<String>() ;
+        chat.add(response);
         switch(exchange.getRequestMethod()){
             case "GET":
+                ByteBuffer buffer = StandardCharsets.UTF_8.encode(response) ;
+                byte [] bytes = new byte [buffer.remaining()];
+                buffer.get(bytes);
+                exchange.getResponseHeaders().set("Content - Type ","text / html ; charset =UTF -8");
+                try {
+                    exchange.sendResponseHeaders (HTTP_OK,bytes.length) ;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                exchange.getResponseBody().write(bytes) ;
+                exchange.close () ;
+                break;
+            case "POST":
+
+
 
         }
-        String response = "Server is running.";
+       /* for (int i = 0; i < chat.size(); i++){
+
+        }*/
         // create utf 8 buffer
-        ByteBuffer buffer = StandardCharsets.UTF_8.encode(response) ;
-        byte [] bytes = new byte [buffer.remaining()];
-        System.out.println(buffer.toString());
-        buffer.get(bytes);
-        exchange.getResponseHeaders().set("Content - Type ","text / html ; charset =UTF -8");
-        try {
-            exchange.sendResponseHeaders (HTTP_OK,bytes.length) ;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        exchange.getResponseBody().write(bytes) ;
-        exchange.close () ;
+
+
     }
 
 
